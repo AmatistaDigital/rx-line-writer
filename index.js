2f1e92d258b8419b76bd003cfdc757babb98c432
Alberto Cole <acpii2005@gmail.com>
2016-10-30 14:30:56 -0600

    printing fixes, npm update

--- index.js
+++ index.js
@@ -4,63 +4,70 @@ const _ = require('lodash');
 const db = require('./db');
 const chalk = require('chalk');
 
+const randomColor = [
+  'red',
+  'black',
+  'red',
+  'green',
+  'yellow',
+  'blue',
+  'magenta',
+  'cyan',
+  'white',
+  'gray',
+];
+
 const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min)) + min;
 
 const rl = readline.createInterface({
-  input : require('fs').createReadStream('../BDBM_01042016/BMFD016.TXT'),
+  input : require('fs').createReadStream('../BDBM_01042016/BMFD033.TXT'),
 });
 const saveBatch = (list) => {
-  const fieldNames = ['tipoBien',
-  'claseBien',
-  'codigoBien',
-  'numeroBien',
-  'codigoMarca',
-  'anoFabricacion',
-  'estilo',
-  'codigoCategoria',
-  'tipoBienInmueble',
-  'numeroChasis',
-  'numeroSerie',
-  'codigoCarroceria',
-  'capacidad',
-  'pesoNeto',
-  'pesoBruto',
-  'pesoRemolque',
-  'pesoVacio',
-  'codigoColor',
-  'codigoTraccion',
-  'VIN',
-  'tipoCabina',
-  'tipoTecho',
-  'numeroEjes',
-  'longitudBien',
-  'refaccionado',
-  'convertido',
-  'timeStamp',
-  'claseTributaria'];
+  const fieldNames = [
+    'tomo',
+    'asiento',
+    'secuencia',
+    'numero',
+    'tomoTrasladoAHistoria',
+    'asientoTrasladoAHistoria',
+    'secuenciaTrasladoAHistoria',
+    'tipoBien',
+    'claseBien',
+    'codigoBien',
+    'numeroBien',
+    'numeroDerecho',
+    'tomoGravamenAfectado',
+    'asientoGravamenAfectado',
+    'secuenciaGravamenAfectado',
+    'codigoTipo',
+    'codigoGrado',
+    'tomoConstitucion',
+    'asientoConstitucion',
+    'secuenciaConstitucion',
+    'numeroFinca',
+    'anoAgricola',
+    'codigoEstado',
+    'proporcionMontoTotal',
+    'descrpicion',
+    'timeStamp',
+  ];
   db.saveBatch({
     list : _.map(list, (line) =>{
       const splitLine = line.split(';');
       const object = {};
       _.each(fieldNames, (fieldName, i) => {
-        object[fieldName] = splitLine[i].replace(/["']/g, '').trim();
+        if (splitLine[i]) {
+          object[fieldName] = splitLine[i].replace(/["']/g, '').trim();
+        }
       });
       return object;
     }),
     mongoURL       : 'mongodb://localhost:27017/helix_nebula',
-    collectionName : 'bienmueble.vehiculo',
+    collectionName : 'bienmueble.garantia.historico',
   }).then(() => {
-    // eslint-disable-next-line
     const randomNumber = getRandomInt(0, 10);
-    if (randomNumber < 2) {
-      console.log(chalk.green('batch saved'));
-    } else if (randomNumber < 5 ) {
-      console.log(chalk.red('batch saved'));
-    } else if (randomNumber < 7 ) {
-      console.log(chalk.blue('batch saved'));
-    } else {
-      console.log(chalk.yellow('batch saved'));
-    }
+    // eslint-disable-next-line
+    console.log(chalk[randomColor[randomNumber]]('batch saved'));
   }).catch((err) => {
     // eslint-disable-next-line
     console.log('batch ERROR');
